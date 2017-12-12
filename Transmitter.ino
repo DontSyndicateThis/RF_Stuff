b1fc773ed51609a7a39671efe3aa563bf28599cc
Leblanc-Royer <leblancroyere@wit.edu>
2017-12-12 17:41:54 -0500

    Transmitter

---
+++ Transmitter.ino
@@ -0,0 +1,16 @@
+#include <RCSwitch.h>
+int ledKill = 3; 
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledKill,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(ledKill, LOW);
+
+  
+
+}
